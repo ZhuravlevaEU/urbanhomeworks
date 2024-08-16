62ba0d86ec29ae065ed6d246a2fcc416f0784a25
Elena Zhuravleva <schedrova@gmail.com>
2024-08-16 23:01:48 +0300

    Create m1_dz4.py

---
+++ m1_dz4.py
@@ -0,0 +1,8 @@
+# модуль 1 задание 4
+my_string=input("Привет! Как дела? ")
+print(len(my_string))
+print(my_string .upper())
+print(my_string .lower())
+print(my_string.replace(' ', ''))
+print(my_string[0])
+print(my_string[-1])
