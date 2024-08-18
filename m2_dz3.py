a8fe4598edf243d308236f58e44ed18d125139dc
Elena Zhuravleva <schedrova@gmail.com>
2024-08-18 23:18:40 +0300

    Create m2_dz3.py

---
+++ m2_dz3.py
@@ -0,0 +1,10 @@
+# Задача "Нули ничто, отрицание недопустимо!"
+a = 0
+my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+while (my_list[a]) == True:
+    if my_list[a] > 0:
+        print(my_list[a])
+        continue
+    else:
+        print('Все')
+    break
