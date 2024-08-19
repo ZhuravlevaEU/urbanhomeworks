05cc6cf33cc3f614f8fd9a384aa0c7eb7f239fe0
Elena Zhuravleva <schedrova@gmail.com>
2024-08-19 22:30:57 +0300

    Create m2_dz4.py

---
+++ m2_dz4.py
@@ -0,0 +1,15 @@
+# Цикл for. Элементы списка
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+primes = []
+not_primes = []
+i = [0]
+for i in numbers[i]:
+    for i in range(numbers[i]):
+        if numers[i] % numbers[i] and len(numbers):
+            primes.append[i] = numbers[i]
+            print(primes[i])
+        elif not_primes.append[i] = numbers[i]
+            print(primes[i])
+        else:
+    continue
+continue
