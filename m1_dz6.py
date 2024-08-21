80d026aee64e681b09e05dcefe8f2fba8c1b18ef
Elena Zhuravleva <schedrova@gmail.com>
2024-08-21 15:46:50 +0300

    Update m1_dz6.py

--- m1_dz6.py
+++ m1_dz6.py
@@ -1,3 +1,4 @@
+# задание 6 Работа со словарями и множествами
 # создаю словарь
 my_dict = {'Denis': 1999, 'Anton': 2000, 'Alex': 2001}
 print(my_dict)
@@ -7,10 +8,16 @@ print(my_dict['Denis'])
 my_dict.update({'Pol': 2000, 'Kat': 2002})
 print(my_dict)
 # удаление 1й пары по ключу
-print(my_dict.pop('Denis'))
+print(my_dict.pop('Denis')) # функция pop убирает ключ и показывает его знач
+print(my_dict)
+# функция get возвращает
+print(my_dict.get('Denis')) # функция убирает ключ и показывает его знач
+print(my_dict)
 # создаю множество
+my_set = 0
 my_set = {1, 2, 3, 1, 2, 3, True, False, 'Привет!'}
 print(my_set)
+#
 # добавление 2х элементов
 my_set.update([5, 7])
 print(my_set)
