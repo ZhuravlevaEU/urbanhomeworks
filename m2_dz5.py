0b91fa13528357500cf95f8c44bf2cba2b93af76
Elena Zhuravleva <schedrova@gmail.com>
2024-08-21 21:50:46 +0300

    m2_dz5.py

--- m2_dz5.py
+++ m2_dz5.py
@@ -1,21 +1,21 @@
 # Задача "Матрица воплоти":
-
 def get_matrix(n, m, value):
-    result1 = get_matrix(2, 2, 10)
-    result2 = get_matrix(3, 5, 42)
-    result3 = get_matrix(4, 2, 13)
     matrix = []
     index1 = 0
     for index1 in n:
         # добавьте пустой список в matrix???
-        matrix1 = [matrix]
         index2 = 0
         for index2 in m:
-            matrix1 = [value]
+            matrix = [value]
             get_matrix(n, m, value)
+            print(matrix)
             break
         break
-# верните значение переменной matrix?????
+    return n, m, value
+
+result1 = get_matrix(2, 2, 10)
+result2 = get_matrix(3, 5, 42)
+result3 = get_matrix(4, 2, 13)
 print(result1)
 print(result2)
 print(result3)
