7398bbbfc9c1e4e0e715de8c540d3de26dc5ac50
Elena Zhuravleva <schedrova@gmail.com>
2024-08-21 10:44:52 +0300

    Update m2_dz3.py

--- m2_dz3.py
+++ m2_dz3.py
@@ -1,11 +1,12 @@
 # Задача "Нули ничто, отрицание недопустимо!"
 my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 index = 0
+my_list_1 = []
 while index < len(my_list):
-    if my_list[index] > 0:
-        print(my_list.append(index))
+    if my_list[index] >= 0:
+        my_list_1.append(my_list[index])
+        print(my_list_1[index])
         index += 1
         continue
     else:
         break
-    print('Все')
