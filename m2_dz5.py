4d8563ce33012428f79bff402f00a793e75daefd
Elena Zhuravleva <schedrova@gmail.com>
2024-08-21 22:19:38 +0300

    Update m2_dz5.py

--- m2_dz5.py
+++ m2_dz5.py
@@ -4,13 +4,18 @@ def get_matrix(n, m, value):
     m = int(input('Введите количество столбцов матрицы (2, 5, 2)')) - 1
     value = int(input('Введите значение переменной (10, 42, 13)'))
     index1 = 0
-    for index1 in n:
-        # добавьте пустой список в matrix???
+    for index1 in range(n):
+        # добавляю пустой список в matrix
         matrix = []
         index2 = 0
-        for index2 in m:
+        for index2 in range(m):
             matrix = [n, m, value]
             break
         break
     print(matrix)
     return n, m, value
+
+n = int(input('Введите количество строк матрицы (2, 3, 4)')) - 1
+m = int(input('Введите количество столбцов матрицы (2, 5, 2)')) - 1
+value = int(input('Введите значение переменной (10, 42, 13)'))
+get_matrix(n, m, value)
