0d35013789fde0181cc1ef94a9f59dbd94d80614
Elena Zhuravleva <schedrova@gmail.com>
2024-08-21 15:59:54 +0300

    Update m1_dz5.py

--- m1_dz5.py
+++ m1_dz5.py
@@ -2,6 +2,10 @@
 tuple_ = (1, 2, 3, 4, 5, False, 'комар')
 immutable_var = tuple_
 print(immutable_var)
+# заменю элемент кортежа
+# tuple_[0] = 9 TypeError: 'tuple' object does not support item assignment
+# кортеж представляет собой единый объект, не подлежащий поелементому преобразованию
+print(immutable_var)
 # заменяю первый элемент списка
 a, b, c, d, e, f, g = tuple_
 a = 7
