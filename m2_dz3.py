7f44784b9ad92e6361200a6e50fa9b015b60bd58
Elena Zhuravleva <schedrova@gmail.com>
2024-08-22 22:25:08 +0300

    Update m2_dz3.py

--- m2_dz3.py
+++ m2_dz3.py
@@ -3,7 +3,7 @@ my_list = [42, 69, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
 index = 0
 my_list_1 = []
 while index < len(my_list):
-    if my_list[index] >= 0:
+    if my_list[index] > 0:
         my_list_1.append(my_list[index])
         print(my_list_1[index])
         index += 1
