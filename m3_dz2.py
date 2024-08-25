0185d1f78e8bcd8142db71949d41b9aeddddab50
Elena Zhuravleva <schedrova@gmail.com>
2024-08-25 19:52:47 +0300

    Update m3_dz2.py

--- m3_dz2.py
+++ m3_dz2.py
@@ -1,18 +1,18 @@
 # Рассылка писем
-def send_email (messege, recipient, *, sender):
+def send_email(messege, recipient, *, sender):
     if i in seach_addressee:
         print('Письмо успешно отправлено с адреса university.help@gmail.com')
-    elif i == '@mail.ru' or i == '@ya.ru' or i == '@bk.ru':
-                print('НЕСТАНДАРТНЫЙ ПОЛУЧАТЕЛЬ! Получатель urban.fan@mail.ru')
+    elif i == '.ru' or i == '.com' or i == '.net':
+        print('НЕСТАНДАРТНЫЙ ПОЛУЧАТЕЛЬ! Получатель urban.fan@mail.ru')
     elif i == 'mail.uk':
-            print('Невозможно отправить письмо с адреса urban.teacher@mail.uk')
-    elif recipient = sender:
-            print('Нельзя отправить письмо самому себе!')
-        else:
+        print('Невозможно отправить письмо с адреса urban.teacher@mail.uk')
+    elif recipient == sender:
+        print('Нельзя отправить письмо самому себе!')
+    else:
 
 messege = input('Введите текст сообщения: ')
 recipient = input('Получатель: ')
 sender = 'urban.info@gmail.com'
 i = '@gmail.com'
 
-send_email (messege, recipient, *, sender)
+send_email(messege, recipient, *, sender)
