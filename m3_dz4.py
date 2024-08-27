210d044fcf97fae551f20774f408980d23969e60
Elena Zhuravleva <schedrova@gmail.com>
2024-08-27 21:01:27 +0300

    Update m3_dz4.py

--- m3_dz4.py
+++ m3_dz4.py
@@ -1,10 +1,11 @@
-# Задача "Однокоренные" (произв число параметров
+# Задача "Однокоренные" (произв число параметров)
 
 def single_root_words(root_word, *other_words):
-    if root_word in lower(other_words) or other_words in lower(root_word):
+    if root_word in other_words.lower() or other_words in root_word.lower():
         same_words = []
         print(same_words)
     else:
+
 
 result1 = single_root_words('rich', 'richiest', 'orichalcum', 'cheers', 'richies')
 result2 = single_root_words('Disablement', 'Able', 'Mable', 'Disable', 'Bagel')
