0511b03d6ae6e7d0b630485983935bf97663d82c
Elena Zhuravleva <schedrova@gmail.com>
2024-08-27 20:39:15 +0300

    Update m3_dz2.py

--- m3_dz2.py
+++ m3_dz2.py
@@ -1,18 +1,20 @@
 # Рассылка писем
-def send_email(messege, recipient, *, sender):
-    if i in seach_addressee:
-        print('Письмо успешно отправлено с адреса university.help@gmail.com')
-    elif i == '.ru' or i == '.com' or i == '.net':
-        print('НЕСТАНДАРТНЫЙ ПОЛУЧАТЕЛЬ! Получатель urban.fan@mail.ru')
-    elif i == 'mail.uk':
-        print('Невозможно отправить письмо с адреса urban.teacher@mail.uk')
+def send_email(messege: str, recipient: str, sender='urban.info@gmail.com'):
+    if '@' not in recipient or '@' not in sender\
+        or not recipient.endswith(('.com', '.ru', '.net'))\
+        or sendler.endswith(('.com', '.ru', '.net')):
+        print(f"Невозможно отправить сообщение с адреса {sender} на адрес {recipient}")
     elif recipient == sender:
         print('Нельзя отправить письмо самому себе!')
+    elif '.uk' in recipient:
+        print(f'Недопустимый адрес: {recipient}')
+    elif sender != 'urban.info@gmail.com':
+        print(f"НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ: {sender}")
     else:
+        print(f'Сообщение: {messege} отправлено с адреса: {sender} получателю: {recipient}')
+
 
 messege = input('Введите текст сообщения: ')
 recipient = input('Получатель: ')
-sender = 'urban.info@gmail.com'
-i = '@gmail.com'
 
-send_email(messege, recipient, *, sender)
+send_email(messege, recipient, 'urban.info@gmail.com')
