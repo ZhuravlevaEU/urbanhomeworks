b51809de820d9a31cd0dc1528ebed7f62586acf2
Elena Zhuravleva <schedrova@gmail.com>
2024-08-27 20:54:25 +0300

    Update m2_dz6.py

--- m2_dz6.py
+++ m2_dz6.py
@@ -2,12 +2,13 @@ import random
 
 number = random.randint(3, 20)
 print(number)
-list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-i = 2
+list_ = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+str = ''
 
-for i in list:
-    if (list[i-2] + list[i-1]) % number == 0:
-        print(list[i-2], list[i-1])
-    else:
-        i += 1
-        continue
+for i in range(1, number):
+    for j in range(i + 1, number):
+        if number % (i + j) == 0:
+            str = str[i] + str[j]
+            print(number, '-', str)
+        else:
+            print('задача решена')
