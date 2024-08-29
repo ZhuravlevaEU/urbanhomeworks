fc0d4a9539bdbca4e71fc4719cf46540510d32f3
Elena Zhuravleva <schedrova@gmail.com>
2024-08-29 21:31:10 +0300

    Update m2_dz6.py

--- m2_dz6.py
+++ m2_dz6.py
@@ -3,12 +3,12 @@ import random
 number = random.randint(3, 20)
 print(number)
 list_ = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-str = ''
+str_ = ''
 
 for i in range(1, number):
     for j in range(i + 1, number):
         if number % (i + j) == 0:
-            str = str[i] + str[j]
-            print(number, '-', str)
-        else:
-            print('задача решена')
+            str_ = str(i) + str(j)
+    pass
+
+print(number, '-', str_)
