cec9686b5bdcf22d3fac106943ceafa7b85c3e67
Elena Zhuravleva <schedrova@gmail.com>
2024-08-29 21:44:52 +0300

    Update m3_dz5.py

--- m3_dz5.py
+++ m3_dz5.py
@@ -1,13 +1,12 @@
 # Рекурсивное умножение цифр
-def get_multiplied_digits(n):
-    if len(str_n) > 1:
-        return str_n
+def get_multiplied_digits(number):
+    if len(str_number) > 1:
+        return str_number
     else:
-        return n * get_multiplied_digits(int(str_n[n]))
-    print(get_multiplied_digits(n))
+        return number * get_multiplied_digits(int(str_number[number]))
+    print(get_multiplied_digits(number))
 
-n = (1, 2, 3, 4, 5, 6, 7, 8, 9)
-str_n = int(n)
-result = get_multiplied_digits(40203)
+number = 40203
+str_number = str(int(number))
+result = get_multiplied_digits(number)
 print(result)
-
