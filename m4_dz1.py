d7bcef32bbde67bcf73e94ad8ca91dc00290a6b2
Elena Zhuravleva <schedrova@gmail.com>
2024-09-11 22:12:35 +0300

    Create m4_dz1.py

---
+++ m4_dz1.py
@@ -0,0 +1,12 @@
+from fake_math import divide as fm
+from true_math import divide as fm
+
+
+result1 = fm(69, 3)
+result2 = fm(3, 0)
+result3 = fm(49, 7)
+result4 = fm(15, 0)
+print(result1)
+print(result2)
+print(result3)
+print(result4)
