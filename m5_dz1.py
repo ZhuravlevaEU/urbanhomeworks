eaed1cf95f449f4da22a66f0d4a1c69a257d182b
Elena Zhuravleva <schedrova@gmail.com>
2024-09-28 13:41:45 +0300

    Create m5_dz1.py

---
+++ m5_dz1.py
@@ -0,0 +1,25 @@
+class House:
+
+
+def __init__(self, name, number_of_floor):
+    self.name = name
+    self.number_of_floor = number_of_floor
+
+def self.go_tu(new_floor):
+    for new_floor = int (number_of_floor)
+        if new_floor > self.number_of_floor or new_floor < self.number_of_floor
+        print(f'Название {self.name} этажность {number_of_floor}')
+        else
+            print('Такого этажа нет')
+
+
+
+h1 = House('ЖК Горский', 18)
+h2 = House('Домик в деревне', 2)
+h1.go_to(5)
+h2.go_to(10)
+
+self.go_tu(new_floor)
+
+print(h1.name, h1.number_of_floor)
+print(h2.name, h2.number_of_floor)
