535e1c1d66c54cd93fd5b3dc728b323d66624cb2
Elena Zhuravleva <schedrova@gmail.com>
2024-10-07 18:43:47 +0300

    Update m2_dz4.py

--- m2_dz4.py
+++ m2_dz4.py
@@ -3,17 +3,17 @@ numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
 primes = []
 not_primes = []
 
-for index in numbers:
-    if index < 2:
+for index, elem in enumerate(numbers):
+    if elem < 2:
         continue
-
-    for j in range(2, index):
-        if index % j != 0:
-            primes.append(index)
-            print(primes)
+    is_prime = True
+    for div in range(2, elem):
+        if elem % div == 0:
+            not_primes.append(elem)
+            is_prime = False
             break
-    else:
-        not_primes.append(index)
-        print(not_primes)
+    if is_prime:
+        primes.append(elem)
 
-
+print(primes)
+print(not_primes)
