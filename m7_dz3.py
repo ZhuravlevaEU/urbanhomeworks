d8dcca5fcb6b62bfb546e203b67e5be9d0c85957
Elena Zhuravleva <schedrova@gmail.com>
2024-10-14 22:21:46 +0300

    Update m7_dz3.py

--- m7_dz3.py
+++ m7_dz3.py
@@ -2,28 +2,31 @@
 
 class WordsFinder:
     def __init__(self, *file_names):  # неограниченное число Объектов класса
-        self.file_name = file_names  # запишем объекты в список
+        self.file_names = file_names  # запишем объекты в список
 
-
-def get_all_words(self):  # - подготовительный метод, который возвращает cловарь
-    all_words = {}  # формируем пустой словарь
-    for file_name in self.file_names:
-        with open(file_name, 'r') as file:  # перебераем названия файлов и открываем каждый из них
-            line = file.read().lower()  # считывая строки переводим все символы в нижний регистр
-            for punctuation in [',', '.', '=', '!', '?', ';', ':', ' - ']:
-                line = line.replace(punctuation, '')  # удаляем знаки препинания из строки
-            words = line.split()  # разделяем строку на части по пробелам
-            all_words[file_name] = words  # запишем в словарь полученные слова, где ключ — название файла,
-            # а значение — список из слов этого файла
-            print(all_words[file_name])  # печать значения по ключу
-    return all_words
+    def get_all_words(self):  # - создаем cловарь
+        all_words = {}  # формируем пустой словарь
+        l = ""
+        for file_name in self.file_names:
+            # перебераем названия файлов и открываем каждый из них
+            with open(file_name, 'r', encoding='utf-8') as file:
+                line = file.read().lower()  # считывая строки переводим все символы в нижний регистр
+                for punctuation in [',', '.', '=', '!', '?', ';', ':', ' - ']:
+                    line = line.replace(punctuation, '')  # удаляем знаки препинания из строки
+                words = line.split()  # разделяем строку на части по пробелам
+                all_words[file_name] = words  # запишем в словарь полученные слова, где ключ — название файла,
+                # а значение — список из слов этого файла
+                print(all_words[file_name])  # печать значения по ключу
+        return all_words
 
     def find(self, word):  # метод возвращает словарь, где word - искомое слово, где ключ - название файла,
-        # значение - позиция первого такого слова в списке слов этого файла
+        # значение - позиция первого такого слова в списке слов этого файла;
         dict_ = {}  # создаем словарь
-        for name, words in self.get_all_words().items():  # перебираем по ключу (названию) и значению (списоку слов);
+        # перебираем по ключу (названию) и значению (списоку слов);
+        for name, words in self.get_all_words().items():
             # item() - одновременная итерация
-            if word.lower() in words:  # если строка содержит слово
+            # если строка содержит слово
+            if word.lower() in words:
                 # значение ключа словаря установим в индекс слова в списке слов, увеличенный на 1****
                 dict_[name] = words.index(word.lower()) + 1
                 # возвращаем словарь, где ключ - название файла, значение - позиция первого такого
