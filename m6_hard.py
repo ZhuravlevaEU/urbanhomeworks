423eab5062d46b710b1a8a0cdd478163364569d3
Elena Zhuravleva <schedrova@gmail.com>
2024-10-14 21:05:12 +0300

    Update m6_hard.py

--- m6_hard.py
+++ m6_hard.py
@@ -1,77 +1,76 @@
 # Дополнительное задание 6 модуля. Наследование классов
-
+import math
 class Figure: #класс фигуры
     sides_count = 0
 
-    def __init__(self, __sides, __color, filled):
+    def __init__(self, color:tuple[int, int, int], *sides, filled=False):
         self.__sides = sides  #список сторон - целые числа
-        self.__color = colors #список цветов в формате RGB
-        self.filled = filleds #закрашенный/ не закрашенный
+        self.__color = color #список цветов в формате RGB
+        self.filled = filled #закрашенный/ не закрашенный
 
-    def set_color(self):
-        for r in range(1, 255):
-            for g in range(1, 255):
-                for b in range(1, 255):
-                    self.__color(r, g, b) = [rgb]
-        brek
-            print(f' Цвет - прежний')
-
-    def __is_valid_color(self, r,g,b):
-        #проверка цвета
-        if self.__color > 0 and self.__color == int
-            print(f'True')
-        else:
-            print(f'False')
-    def get_color(self):
+    def get_color(self): # вызываем цвет фигуры
         return f'Цвет фигуры {self.__color}'
 
-    def set_sides(self, *new_sides):
-        self.__sides() = сумма всех сторон
-        sides_count
-        """for arg in args:
-            if isinstance(arg, str):
-                total_sum += len(arg)
-            elif isinstance(arg, (int, float)):
-                total_sum += arg
-            elif isinstance(arg, (list, tuple, set)):
-                total_sum += all_sum(*arg)
-            elif isinstance(arg, dict):
-                total_sum += all_sum(*arg.items())
-            elif arg is None:
-                pass
-        return total_sum"""
+    def __is_valid_color(self, r,g,b): # проверяем цвет фигуры
+        valid_values = 0 <= r <= 255 and 0 <= g <= 255 and 0 <= b <= 255
+        valid_types = isinstance(r, int) and isinstance(g, int) and isinstance(b, int)
+        return valid_types and valid_values
 
-    def __is_valid__sides(self, bool):
+    def set_color(self, r, g, b):
+        if self.__is_valid_color(r, g, b):
+            self.__color = r, g, b
+
+    def __is_valid__sides(self, *sides):
+        for side in sides:
+            if not isinstance(side, int) or side <= 0:
+                return False
+            return True
 
     def get_sides(self):
-        return f'Список сторон {self.__sides}'
+        return sum(self.__sides)
 
-    def __len__(self, perimetr):
-        ????
+    def __len__(self):
+        return sum(self.__sides)
+
+    def set_sides(self, *sides):
+        if len(sides) == len(self.__sides):
+            valid_sides = []
+            for side in sides:
+                if self.__is_valid__sides(side):
+                    valid_sides.append(side)
+
+        self.__sides = valid_sides
+
 
 class Circle(Figure):  #класс окружности
     sides_count = 1
-    def __init__(self, __radius):
-        self.__radius = __radius
+
+    def __init__(self, color: tuple[int, int, int], length, filled=False):
+        super().init(color, length, filled=filled)
+        self.radius = length / (2 * math.pi)
 
     def get_square(self, length): # расчет по формуле площадь и длина окружности
+        return len(self) ** 2 / (4 * math.pi)
 
 class Triangle(Figure): #класс треугольники
     sides_count = 3
-    def __init__(self, __a, __b, __c):
-        self.__a = first
-        self.__b = second
-        self.__c = third
+    def __init__(seself, color: tuple[int, int, int], height, *sides, filled=False):
+        super().init(color, *sides, filled=filled)
+        self.height = height
 
     def get_square(self): # расчет по формуле герона
+        p = (len(self) / 2)
+        sides = self.get_sides()
+        return math.sqrt(p * (p - sides[0]) * (p - sides[1]) * (p - sides[2]))
 
 
 class Cube(Figure): #класс кубы
     sides_count = 12
-    def __init__(self, __sades):
-        self.__sades = sides
-    def get_volume(self, sades): # объем куба
-        self.__sades =
+    def __init__(self, color: tuple[int, int, int], side, filled=False):
+        super().init(color, *cube_sides, filled=filled)
+
+    def get_volume(self):
+        return self.get_sides()[0] ** 3
 
 
 circle1 = Circle((200, 200, 100), 10) # (Цвет, стороны)
