d5348d0d8fd7f562bdb7971aa6d2e25bb87a7937
Elena Zhuravleva <schedrova@gmail.com>
2024-10-18 20:42:40 +0300

    Update m8_dz3.py

--- m8_dz3.py
+++ m8_dz3.py
@@ -1,44 +1,60 @@
 # Создание исключений
+class IncorrectCarNumbers(Exception):
+    def __init__(self, message, *args):
+        self.message = message
+
+class IncorrectVinNumbers(Exception):
+    def __init__(self, message, *args):
+        self.message = message
+
 class Car:
+    def __init__(self, model, vin, numbers):
+        self.model = model # название автомобиля
+        if self.__is_valid_vin(vin):
+            self.__vin = vin  # vin-номер автомобиля
+        if self.__is_valid_numbers(numbers):
+            self.__numbers = numbers  # номера автомобиля (строка)
 
-    def __init__(self, model: str, vin: int, numbers: str):
-        self.model = model  # название автомобиля
-        self._vin = vin  # vin номер автомобиля
-        self._numbers = numbers  # номера автомобиля (строка)
-
-    def __is_valid_vin(vin_number):  # проверка на корректность vin-номер
-        if not 1000000 < self._vin < 9999999:
-            raise Car("Vin-номер не корректный")
-        else:
-            print(f"Vin номер: {vin_number})
-        break
+    def __is_valid_vin(vin_numbers):  # проверка на корректность vin-номер
+        if not isinstance(vin_numbers, int):
+            raise IncorrectVinNumbers("Vin-номер не корректный")
+        elif vin_numbers not in range(1000000, 9999999):
+            raise IncorrectVinNumbers('Неверный диапазон для vin номера',
+            f'Диапазон: 1000000-9999999 Ваш Vin номер: {vin_numbers}')
+        return True
 
     def __is_valid_numbers(numbers):  # проверяет на корректность номер автомобиля
         if not isinstance(numbers, str):
             raise IncorrectCarNumbers("Тип номера не корректный")
         elif len(numbers) != 6:
-            raise IncorrectCarNumbers('Неверная длина номера')
-        else:
-            print(f"Номер: {numbers})
-
-
-class ProZero(Exception):
-    def __init__(self, message, extra_info):
-        self.message = message
-        self.extra_info = extra_info
-
-    try:
-        result = f( // // /)
-    print(result)
-    except Car():
-    print("Что-то c vin пошло не так(")
-    print(f"Сообщение об ошибке: {e.message}")
-    print(f"Дополнительная информация: {e.extra_info}")
+            raise IncorrectCarNumbers('Неверная длина номера',
+                                      f'Ваш номер {numbers} содержит не правильное количество цифр')
+        return True
 
 
 try:
-    result = f( // // /)
-    print(result)
-except Car:
-    print("Что-то пошло не так(")
-    print(f("Сообщение об ошибке: не корректный ввод номер машины"))
+  first = Car('Model1', 1000000, 'f123dj')
+except IncorrectVinNumbers as exc:
+  print(exc.message)
+except IncorrectCarNumbers as exc:
+  print(exc.message)
+else:
+  print(f'{first.model} успешно создан')
+
+try:
+  second = Car('Model2', 300, 'т001тр')
+except IncorrectVinNumber as exc:
+  print(exc.message)
+except IncorrectCarNumbers as exc:
+  print(exc.message)
+else:
+  print(f'{second.model} успешно создан')
+
+try:
+  third = Car('Model3', 2020202, 'нет номера')
+except IncorrectVinNumber as exc:
+  print(exc.message)
+except IncorrectCarNumbers as exc:
+  print(exc.message)
+else:
+  print(f'{third.model} успешно создан')
