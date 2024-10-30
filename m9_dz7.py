38e441d9d991cbbb183e66a28875ab708c89e274
Elena Zhuravleva <schedrova@gmail.com>
2024-10-30 12:00:31 +0300

    Create m9_dz7.py

---
+++ m9_dz7.py
@@ -0,0 +1,15 @@
+# задание по теме "Декораторы"
+def my_decorator(is_prime):
+    def is_prime(*args):
+        if sum_three(a, b, c) %  sum_three(a, b, c):
+            print(f'Простое')
+        else:
+            print(f'Составное')
+    result = is_prime(*args)
+    return is_prime
+@my_decorator
+def sum_three(a, b, c)
+    return a + b + c
+
+result = sum_three(2, 3, 6)
+print(result)
