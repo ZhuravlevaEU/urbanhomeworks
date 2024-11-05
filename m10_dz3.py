9e7136aa6dc55ca82e2359774593cd53c7e971a4
Elena Zhuravleva <schedrova@gmail.com>
2024-11-05 13:51:16 +0300

    Update m10_dz3.py

--- m10_dz3.py
+++ m10_dz3.py
@@ -3,17 +3,17 @@ import threading
 import random
 import time
 
-class Bank:
-    counter = 0 # число транзакций пополнения и снятия
-    lock = threading.Lock()
-    def __init__(self, balance):
-        self.balance = bk
 
-    def deposit(self): # пополнение
-        global counter
+class Bank:
+    def __init__(self):
+        self.balance = 0
+        self.lock = threading.Lock()
+
+    def deposit(self):  # пополнение
+        counter = 0
         while counter < 100:
-            bk = random.randint(50, 500) # запрос средств
-            lock.release() # снимаем блок
+            bk = random.randint(50, 500)  # запрос средств
+            self.lock.release()  # снимаем блок
             try:
                 self.balance += bk
                 print(f'Пополнение: {bk}. Баланс: {self.balance}\n')
@@ -23,11 +23,11 @@ class Bank:
                 time.sleep(0.01)
                 counter += 1
 
-    def take(self): # снятие
-        global counter
-        lock.acquire()  # устанавливаем блок
+    def take(self):  # снятие
+        counter = 0
+        self.lock.acquire()  # устанавливаем блок
         while counter < 100:
-            bk = random.randint(50, 500) # запрос средств
+            bk = random.randint(50, 500)  # запрос средств
             print(f'Запрос на {bk}')
             try:
                 if bk <= self.balance:
@@ -51,4 +51,3 @@ thread1.join()
 thread2.join()
 
 print(f'Итоговый баланс: {bk.balance}')
-
