011db371e3006c83846ce1266caa7ada62b9751d
Elena Zhuravleva <schedrova@gmail.com>
2024-11-06 19:15:35 +0300

    Update m10_dz3.py

--- m10_dz3.py
+++ m10_dz3.py
@@ -14,7 +14,7 @@ class Bank:
             bk = random.randint(50, 500)  # запрос средств
             self.balance += bk
             print(f'Пополнение: {bk}. Баланс: {self.balance}\n')
-            if self.lock.locked() and self.balanse > 500:
+            if self.lock.locked() and self.balanсe > 500:
                 self.lock.release()  # снимаем блок
 
             time.sleep(0.01)
@@ -31,15 +31,15 @@ class Bank:
                 self.lock.acquire()  # устанавливаем блок
             time.sleep(0.01)
 
-if __name__ == '__main__':
-    bk = Bank()
-    # Т.к. методы принимают self, в потоки нужно передать сам объект класса Bank
-    thread1 = threading.Thread(target=Bank.deposit, args=(bk,))
-    thread2 = threading.Thread(target=Bank.take, args=(bk,))
 
-    thread1.start()
-    thread2.start()
-    thread1.join()
-    thread2.join()
+bk = Bank()
+# Т.к. методы принимают self, в потоки нужно передать сам объект класса Bank
+thread1 = threading.Thread(target=Bank.deposit, args=(bk,))
+thread2 = threading.Thread(target=Bank.take, args=(bk,))
 
-    print(f'Итоговый баланс: {bk.balance}')
+thread1.start()
+thread2.start()
+thread1.join()
+thread2.join()
+
+print(f'Итоговый баланс: {bk.balance}')
